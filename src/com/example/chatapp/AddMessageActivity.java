06475cfe71745ae20fd50a3e9dba76eb091a760c
takamims <takamims@nttdata.co.jp>
2015-09-30 15:53:44 +0900

    AddMessageActivityを追加

---
+++ src/com/example/chatapp/AddMessageActivity.java
@@ -0,0 +1,22 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AddMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_message);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.add_message, menu);
+		return true;
+	}
+
+}
