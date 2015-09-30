cd20d2649023417e311879ec619ad511635acb98
Kensuke Tanaka <tanakakns@nttdata.co.jp>
2015-09-30 17:25:30 +0900

    add .gitignore

--- gen/com/example/chatapp/R.java
+++ gen/com/example/chatapp/R.java
@@ -24,13 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int item1=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int item1=0x7f080004;
         public static final int listView1=0x7f080000;
+        public static final int messageLayout=0x7f080001;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_add_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int message_in_list=0x7f030002;
     }
     public static final class menu {
         public static final int add_message=0x7f070000;
