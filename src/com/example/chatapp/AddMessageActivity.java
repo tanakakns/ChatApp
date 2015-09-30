24b9071283fa66524746389b115b44267f21e663
takamims <takamims@nttdata.co.jp>
2015-09-30 17:08:46 +0900

    AddMessageActivityのボタンを編集

--- src/com/example/chatapp/AddMessageActivity.java
+++ src/com/example/chatapp/AddMessageActivity.java
@@ -3,13 +3,45 @@ package com.example.chatapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddMessageActivity extends Activity {
+
+	TextView tv;
+	Button sendButton;
+	Button cancelButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_message);
+
+		tv = (TextView) findViewById(R.id.inputMessage);
+		sendButton = (Button) findViewById(R.id.inputButton);
+		cancelButton = (Button) findViewById(R.id.cancelButton);
+
+		sendButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String message = tv.getText().toString();
+				sendNewMessage(message);
+				finish();
+			}
+		});
+
+		cancelButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				tv.setText("");
+				finish();
+			}
+		});
+	}
+
+	public void sendNewMessage(String message) {
+		//Send to Server
 	}
 
 	@Override
@@ -19,5 +51,4 @@ public class AddMessageActivity extends Activity {
 		return true;
 	}
 
-	
 }
