598e107a7755103d4b840abf09cbc8f5487549ed
Kensuke Tanaka <tanakakns@nttdata.co.jp>
2015-09-30 17:19:00 +0900

    add message_in_list.xml fix MainActivity

--- src/com/example/chatapp/MainActivity.java
+++ src/com/example/chatapp/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.chatapp;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends Activity {
+	
+	private List messageList = null;
+	private SimpleAdapter adapter = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		messageList = new ArrayList<String>();
+		adapter = new SimpleAdapter(
+				getApplicationContext(),
+				messageList,
+				R.layout.message_in_list,
+				new String[] {"message"}, // from
+                new int[] {R.id.textView1} // to
+				);
+		ListView lv = (ListView) findViewById(R.id.listView1);
+		lv.setAdapter(adapter);
 	}
 
 	@Override
@@ -17,6 +39,37 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item1:
+			Intent intent = new Intent(this, AddMessageActivity.class);
+			startActivityForResult(intent, 0);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		GetMessageSyncTask async = new GetMessageSyncTask();
+		async.execute();
+
+		adapter.notifyDataSetChanged();
+	}
+
+	protected class GetMessageSyncTask extends AsyncTask<Void, Void, List<String>> {
+		
+		@Override
+		protected List<String> doInBackground(Void... arg0) {
+			MessageHttpClient httpClient = new MessageHttpClient();
+			
+			return httpClient.get();
+		}
 	}
 
 }
